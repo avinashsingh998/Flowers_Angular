7a4c1677dfb6ca5ff170d1912d48a956a8db7edb
Avinash Singh <avinashkumarshing63908@gmail.com>
2023-04-08 10:26:34 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { GallaryComponent } from './components/gallary/gallary.component';
+import { ProductComponent } from './components/product/product.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    GallaryComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
